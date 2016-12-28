44ed9ca074dab3363ea3e0fb7cf9321c0dab7c6c
chzishan <ch.zishan.iqbal@gmail.com>
2016-12-28 17:51:24 +0500

    Registry Model fields changed to lower case

--- src/server/models/registry.js
+++ src/server/models/registry.js
@@ -10,13 +10,13 @@ import sequelize from './../utils/sequelize';
 const Registry = sequelize.define('registry', {
   id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, field: 'ID'},
   url: {type: Sequelize.TEXT, field: 'url'},
-  isPublic: {type: Sequelize.BOOLEAN, field: 'is_public'},
-  secure: {type: Sequelize.INTEGER, field: 'secure'},
+  ispublic: {type: Sequelize.BOOLEAN, field: 'is_public'},
+  secure: {type: Sequelize.BOOLEAN, field: 'secure'},
   certificate: {type: Sequelize.TEXT, field: 'certificate'},
-  requiresCert: {type: Sequelize.TEXT, field: 'requires_cert'},
-  userName: {type: Sequelize.TEXT, field: 'user_name'},
+  requirescert: {type: Sequelize.BOOLEAN, field: 'requires_cert'},
+  username: {type: Sequelize.TEXT, field: 'user_name'},
   password: {type: Sequelize.TEXT, field: 'password'},
-  userEmail: {type: Sequelize.BIGINT, field: 'user_email'},
+  useremail: {type: Sequelize.BIGINT, field: 'user_email'},
   }, {
   // don't add the timestamp attributes (updatedAt, createdAt)
   timestamps: false,
