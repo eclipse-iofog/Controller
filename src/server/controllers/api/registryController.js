0dbeb8a64e8d91e500ec11ff7a25ddb12ad1add9
Kate Lukashick <lukashick.kate@gmail.com>
2018-03-12 13:08:33 +0300

    EWC-28 add endpoints for docker registry

---
+++ src/server/controllers/api/registryController.js
@@ -0,0 +1,95 @@
+/**
+ * @author elukashick
+ */
+
+import async from 'async';
+import express from 'express';
+
+const router = express.Router();
+
+import AppUtils from '../../utils/appUtils';
+import logger from '../../utils/winstonLogs';
+import RegistryService from "../../services/registryService";
+import UserService from "../../services/userService";
+
+
+/********************************************* EndPoints ******************************************************/
+
+/*********** GET listRegistryEndPoint (Get: /api/v2/authoring/registry/list) **********/
+const listRegistryEndPoint = function (req, res) {
+    logger.info("Endpoint hit: " + req.originalUrl);
+
+    let params = {},
+        userProps = {
+            userId: 'bodyParams.t',
+            setProperty: 'user'
+        };
+
+    async.waterfall([
+            async.apply(UserService.getUser, userProps, params),
+            async.apply(RegistryService.listRegistry, userProps)
+        ],
+        function (err, result) {
+            AppUtils.sendResponse(res, err, 'registry', params.registry, result);
+        });
+};
+/*********** POST addRegistryEndPoint (Post: /api/v2/authoring/registry/add) **********/
+const addRegistryEndPoint = function (req, res) {
+    logger.info("Endpoint hit: " + req.originalUrl);
+
+    let params = {},
+        userProps = {
+            userId: 'bodyParams.t',
+            setProperty: 'user'
+        },
+        registryProps = {
+            url: 'bodyParams.url',
+            isPublic: 'bodyParams.isPublic',
+            username: 'bodyParams.username',
+            password: 'bodyParams.password',
+            email: 'bodyParams.email'
+        };
+
+    params.bodyParams = req.body;
+    logger.info("Parameters:" + JSON.stringify(params.bodyParams));
+
+    async.waterfall([
+            async.apply(UserService.getUser, userProps, params),
+            async.apply(RegistryService.addRegistry, registryProps)
+        ],
+        function (err, result) {
+            AppUtils.sendResponse(res, err, 'registry', params.registry, result);
+        });
+};
+
+/*********** POST deleteRegistryEndPoint (Post: /api/v2/authoring/registry/delete) **********/
+const deleteRegistryEndPoint = function (req, res) {
+    logger.info("Endpoint hit: " + req.originalUrl);
+
+    let params = {},
+        userProps = {
+            userId: 'bodyParams.t',
+            setProperty: 'user'
+        },
+        registryProps = {
+            id: 'bodyParams.id'
+        };
+
+    params.bodyParams = req.body;
+    logger.info("Parameters:" + JSON.stringify(params.bodyParams));
+
+    async.waterfall([
+            async.apply(UserService.getUser, userProps, params),
+            async.apply(RegistryService.deleteRegistry, registryProps)
+        ],
+        function (err, result) {
+            AppUtils.sendResponse(res, err, 'registry', params.registry, result);
+        });
+};
+
+
+export default {
+    listRegistryEndPoint: listRegistryEndPoint,
+    addRegistryEndPoint: addRegistryEndPoint,
+    deleteRegistryEndPoint: deleteRegistryEndPoint
+};
--- src/server/services/registryService.js
+++ src/server/services/registryService.js
@@ -8,8 +8,23 @@ const findRegistriesByInstanceId = function(props, params, callback) {
   RegistryManager
     .findByInstanceId(instanceId)
     .then(AppUtils.onFind.bind(null, params, props.setProperty, 'Unable to find registries with instanceId: '+instanceId, callback));
-}
+};
+
+const listRegistry = function (props, params, callback) {
+
+};
+
+const addRegistry = function (props, params, callback) {
+
+};
+
+const deleteRegistry = function (props, params, callback) {
+
+};
 
 export default {
-	findRegistriesByInstanceId: findRegistriesByInstanceId
+    findRegistriesByInstanceId: findRegistriesByInstanceId,
+    listRegistry: listRegistry,
+    addRegistry: addRegistry,
+    deleteRegistry: deleteRegistry
 };
