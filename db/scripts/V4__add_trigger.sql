eaeda56a2d17078f962f9087da80d7b63ee1ab85
Kate Lukashick <lukashick.kate@gmail.com>
2018-05-15 17:15:36 +0300

    revert

--- db/scripts/V4__add_trigger.sql
+++ db/scripts/V4__add_trigger.sql
@@ -1,13 +1,3 @@
-/********************************************************************************
- * Copyright (c) 2018 Edgeworx, Inc.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0
- *
- * SPDX-License-Identifier: EPL-2.0
- ********************************************************************************/
-
 CREATE TRIGGER after_element_instance_update AFTER UPDATE ON element_instance
 WHEN old.volume_mappings <> new.volume_mappings OR old.root_host_access <> new.root_host_access
 BEGIN
--- db/scripts/V5__add_image_snapshot.sql
+++ db/scripts/V5__add_image_snapshot.sql
@@ -1,12 +1,2 @@
-/********************************************************************************
- * Copyright (c) 2018 Edgeworx, Inc.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0
- *
- * SPDX-License-Identifier: EPL-2.0
- ********************************************************************************/
-
 ALTER TABLE element_instance ADD image_snapshot	TEXT;
 ALTER TABLE iofog_change_tracking ADD `image_snapshot` BIGINT;
