2dc8c242b6854d525a9d843a9cafbd7ed2e7dcc8
Pankov <Oblivion!2345>
2018-06-29 18:23:03 +0300

    Merge branch 'develop' of https://github.com/ioFog/FogController into EWC-165_self_signed_certs
    # Conflicts:
    #	src/server/services/comsatService.js

--- db/scripts/V9__add_sscerts_to_comsat.sql
+++ db/scripts/V9__add_sscerts_to_comsat.sql
@@ -1,6 +1,4 @@
-DROP TABLE satellite;
-
-create table satellite
+create table satellite_new
 (
 	ID INTEGER
 		primary key
@@ -13,3 +11,11 @@ create table satellite
 	created_at DATETIME not null,
 	updated_at DATETIME not null
 );
+
+INSERT INTO satellite_new (ID, name, domain, public_ip, cert, self_signed_certs, created_at, updated_at)
+  SELECT ID, name, domain, public_ip, cert, 0, created_at, updated_at
+  FROM satellite;
+
+DROP TABLE satellite;
+
+ALTER TABLE satellite_new RENAME TO satellite;
