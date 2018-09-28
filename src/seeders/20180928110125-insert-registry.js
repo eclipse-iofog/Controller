b6a5eb404aea4e4be510a9e3f38a7d13c30a946e
Railag <whenthegroundcavedin@gmail.com>
2018-09-28 15:36:45 +0300

    EWC-211 sequelize seeds for db

---
+++ src/seeders/20180928110125-insert-registry.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+   return queryInterface.bulkInsert('Registry', [
+	   {
+			ID: 1,
+			url: 'registry.hub.docker.com',
+			is_public: 1,
+			secure: 1,
+			certificate: '',
+			requires_cert: 0,
+			user_name: '',
+			password: '',
+			user_email: '',
+			iofog_uuid: null,
+			user_id: null
+	   },
+	   {
+			ID: 2,
+			url: 'from_cache',
+			is_public: 1,
+			secure: 1,
+			certificate: '',
+			requires_cert: 0,
+			user_name: '',
+			password: '',
+			user_email: '',
+			iofog_uuid: null,
+			user_id: null
+	   }
+   ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('Registry', null, {});
+  }
+};
