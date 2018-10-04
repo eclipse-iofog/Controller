c22b2cfd6a3e28705d99a593c1a0ae53acb53d93
Railag <whenthegroundcavedin@gmail.com>
2018-10-04 16:35:34 +0300

    cleanup

--- src/sequelize/managers/baseManager.js
+++ src/sequelize/managers/baseManager.js
@@ -12,6 +12,7 @@
  */
 
 const moment = require('moment');
+const _ = require('underscore');
 
 module.exports = class BaseManager {
 
@@ -41,5 +42,42 @@ module.exports = class BaseManager {
       include: include
     });
   }
+
+  deleteById(id) {
+    return this.findById(id)
+      .then((object) => {
+        if (null == object)
+          return;
+        return object.destroy();
+      });
+  }
+
+  update(newObject) {
+    //find the new content by id
+    return this.findById(newObject.id)
+      .then((dbObject) => {
+        //if id is invalid throw error
+        if (null == dbObject)
+          throw Error('invalid id');
+        //update the properties
+        this.updateObject(dbObject, newObject);
+        //if there are any updated properties
+        //save the changes, otherwise return the object as it is to next step
+        if (!dbObject.changed())
+          return dbObject;
+        return dbObject.save();
+      });
+  }
+
+  //src, destination
+  //destination should be a sequelize object
+  updateObject(destination, source) {
+    _.each(source, (value, key) => {
+      if (key !== 'id' && key !== 'createdAt' && _.has(destination.dataValues, key)) {
+        if (destination[key] !== source[key])
+          destination[key] = source[key];
+      }
+    });
+  };
 
 };
