8ec4c6e1fd2f4d139b3d7a3dbe62378bf8beab2d
Pankov <Oblivion!2345>
2018-10-12 17:20:31 +0300

    Revert "Merge branch 'new-fog-controller' of https://github.com/ioFog/FogController into epankou/feature-catalog-endpoints-EWC-219"
    This reverts commit ab79c7c

--- src/cli/catalog.js
+++ src/cli/catalog.js
@@ -20,7 +20,7 @@ const AppHelper = require('../helpers/app-helper');
 const AuthDecorator = require('../decorators/cli-decorator');
 
 const JSON_SCHEMA =
-	`  name: string
+  `  name: string
   description: string
   category: string
   publisher: string
@@ -46,27 +46,141 @@ class Catalog extends BaseCLIHandler {
 
     this.name = constants.CMD_CATALOG
     this.commandDefinitions = [
-      { name: 'command', defaultOption: true, group: [constants.CMD] },
-      { name: 'file', alias: 'f', type: String, description: 'Catalog item settings JSON file', group: [constants.CMD_ADD, constants.CMD_UPDATE] },
-      { name: 'item-id', alias: 'i', type: String, description: 'Catalog item ID', group: [constants.CMD_UPDATE, constants.CMD_REMOVE, constants.CMD_INFO] },
-      { name: 'name', alias: 'n', type: String, description: 'Catalog item name', group: [constants.CMD_UPDATE, constants.CMD_ADD] },
-      { name: 'description', alias: 'd', type: String, description: 'Catalog item description', group: [constants.CMD_UPDATE, constants.CMD_ADD] },
-      { name: 'category', alias: 'c', type: String, description: 'Catalog item category', group: [constants.CMD_UPDATE, constants.CMD_ADD] },
-      { name: 'x86-image', alias: 'x', type: String, description: 'x86 docker image name', group: [constants.CMD_UPDATE, constants.CMD_ADD] },
-      { name: 'arm-image', alias: 'a', type: String, description: 'ARM docker image name', group: [constants.CMD_UPDATE, constants.CMD_ADD] },
-      { name: 'publisher', alias: 'p', type: String, description: 'Catalog item publisher name', group: [constants.CMD_UPDATE, constants.CMD_ADD] },
-      { name: 'disk-required', alias: 's', type: Number, description: 'Amount of disk required to run the microservice (MB)', group: [constants.CMD_UPDATE, constants.CMD_ADD] },
-      { name: 'ram-required', alias: 'r', type: Number, description: 'Amount of RAM required to run the microservice (MB)', group: [constants.CMD_UPDATE, constants.CMD_ADD] },
-      { name: 'picture', alias: 't', type: String, description: 'Catalog item picture', group: [constants.CMD_UPDATE, constants.CMD_ADD] },
-      { name: 'public', alias: 'P', type: Boolean, description: 'Public catalog item', group: [constants.CMD_UPDATE, constants.CMD_ADD] },
-      { name: 'private', alias: 'V', type: Boolean, description: 'Private catalog item', group: [constants.CMD_UPDATE, constants.CMD_ADD] },
-      { name: 'registry-id', alias: 'g', type: Number, description: 'Catalog item docker registry ID', group: [constants.CMD_UPDATE, constants.CMD_ADD] },
-      { name: 'input-type', alias: 'I', type: String, description: 'Catalog item input type', group: [constants.CMD_UPDATE, constants.CMD_ADD] },
-      { name: 'input-format', alias: 'F', type: String, description: 'Catalog item input format', group: [constants.CMD_UPDATE, constants.CMD_ADD] },
-      { name: 'output-type', alias: 'O', type: String, description: 'Catalog item output type', group: [constants.CMD_UPDATE, constants.CMD_ADD] },
-      { name: 'output-format', alias: 'T', type: String, description: 'Catalog item output format', group: [constants.CMD_UPDATE, constants.CMD_ADD] },
-      { name: 'config-example', alias: 'X', type: String, description: 'Catalog item config example', group: [constants.CMD_UPDATE, constants.CMD_ADD] },
-      { name: 'user-id', alias: 'u', type: Number, description: 'User\'s id', group: [constants.CMD_ADD] },
+      {name: 'command', defaultOption: true, group: [constants.CMD]},
+      {
+        name: 'file',
+        alias: 'f',
+        type: String,
+        description: 'Catalog item settings JSON file',
+        group: [constants.CMD_ADD, constants.CMD_UPDATE]
+      },
+      {
+        name: 'item-id',
+        alias: 'i',
+        type: String,
+        description: 'Catalog item ID',
+        group: [constants.CMD_UPDATE, constants.CMD_REMOVE, constants.CMD_INFO]
+      },
+      {
+        name: 'name',
+        alias: 'n',
+        type: String,
+        description: 'Catalog item name',
+        group: [constants.CMD_UPDATE, constants.CMD_ADD]
+      },
+      {
+        name: 'description',
+        alias: 'd',
+        type: String,
+        description: 'Catalog item description',
+        group: [constants.CMD_UPDATE, constants.CMD_ADD]
+      },
+      {
+        name: 'category',
+        alias: 'c',
+        type: String,
+        description: 'Catalog item category',
+        group: [constants.CMD_UPDATE, constants.CMD_ADD]
+      },
+      {
+        name: 'x86-image',
+        alias: 'x',
+        type: String,
+        description: 'x86 docker image name',
+        group: [constants.CMD_UPDATE, constants.CMD_ADD]
+      },
+      {
+        name: 'arm-image',
+        alias: 'a',
+        type: String,
+        description: 'ARM docker image name',
+        group: [constants.CMD_UPDATE, constants.CMD_ADD]
+      },
+      {
+        name: 'publisher',
+        alias: 'p',
+        type: String,
+        description: 'Catalog item publisher name',
+        group: [constants.CMD_UPDATE, constants.CMD_ADD]
+      },
+      {
+        name: 'disk-required',
+        alias: 's',
+        type: Number,
+        description: 'Amount of disk required to run the microservice (MB)',
+        group: [constants.CMD_UPDATE, constants.CMD_ADD]
+      },
+      {
+        name: 'ram-required',
+        alias: 'r',
+        type: Number,
+        description: 'Amount of RAM required to run the microservice (MB)',
+        group: [constants.CMD_UPDATE, constants.CMD_ADD]
+      },
+      {
+        name: 'picture',
+        alias: 't',
+        type: String,
+        description: 'Catalog item picture',
+        group: [constants.CMD_UPDATE, constants.CMD_ADD]
+      },
+      {
+        name: 'public',
+        alias: 'P',
+        type: Boolean,
+        description: 'Public catalog item',
+        group: [constants.CMD_UPDATE, constants.CMD_ADD]
+      },
+      {
+        name: 'private',
+        alias: 'V',
+        type: Boolean,
+        description: 'Private catalog item',
+        group: [constants.CMD_UPDATE, constants.CMD_ADD]
+      },
+      {
+        name: 'registry-id',
+        alias: 'g',
+        type: Number,
+        description: 'Catalog item docker registry ID',
+        group: [constants.CMD_UPDATE, constants.CMD_ADD]
+      },
+      {
+        name: 'input-type',
+        alias: 'I',
+        type: String,
+        description: 'Catalog item input type',
+        group: [constants.CMD_UPDATE, constants.CMD_ADD]
+      },
+      {
+        name: 'input-format',
+        alias: 'F',
+        type: String,
+        description: 'Catalog item input format',
+        group: [constants.CMD_UPDATE, constants.CMD_ADD]
+      },
+      {
+        name: 'output-type',
+        alias: 'O',
+        type: String,
+        description: 'Catalog item output type',
+        group: [constants.CMD_UPDATE, constants.CMD_ADD]
+      },
+      {
+        name: 'output-format',
+        alias: 'T',
+        type: String,
+        description: 'Catalog item output format',
+        group: [constants.CMD_UPDATE, constants.CMD_ADD]
+      },
+      {
+        name: 'config-example',
+        alias: 'X',
+        type: String,
+        description: 'Catalog item config example',
+        group: [constants.CMD_UPDATE, constants.CMD_ADD]
+      },
+      {name: 'user-id', alias: 'u', type: Number, description: 'User\'s id', group: [constants.CMD_ADD]},
     ]
     this.commands = {
       [constants.CMD_ADD]: 'Add a new catalog item.',
@@ -78,18 +192,18 @@ class Catalog extends BaseCLIHandler {
   }
 
   async run(args) {
-    const catalogCommand = this.parseCommandLineArgs(this.commandDefinitions, { argv: args.argv })
+    const catalogCommand = this.parseCommandLineArgs(this.commandDefinitions, {argv: args.argv})
 
     switch (catalogCommand.command.command) {
       case constants.CMD_ADD:
-        try{
+        try {
           const catalogItem = catalogCommand[constants.CMD_ADD];
 
           const createCatalogItemWrapped = AuthDecorator.prepareUser(createCatalogItem);
           createCatalogItemWrapped(catalogItem);
 
         } catch (error) {
-	        logger.error(error.message);
+          logger.error(error.message);
         }
         return
       case constants.CMD_UPDATE:
@@ -119,51 +233,50 @@ class Catalog extends BaseCLIHandler {
   }
 }
 
-const createCatalogItem = async function(obj, user) {
-	const item = obj.file
+const createCatalogItem = async function (obj, user) {
+  const item = obj.file
     ? JSON.parse(fs.readFileSync('file', 'utf8'))
     : _createCatalogItemObject(obj);
 
-	logger.info(JSON.stringify(item));
+  logger.info(JSON.stringify(item));
 
-	await CatalogItemService.createCatalogItem(item, user);
-	logger.info('Catalog item created successfully.');
+  await CatalogItemService.createCatalogItem(item, user);
+  logger.info('Catalog item created successfully.');
 }
 
-const _createCatalogItemObject = function(catalogItem) {
-	return {
-		name: catalogItem.name,
-		description: catalogItem.description,
-		category: catalogItem.category,
-		configExample: catalogItem.config-example,
-		publisher: catalogItem.publisher,
-		diskRequired: catalogItem.disk-required,
-		ramRequired: catalogItem.ram-required,
-		picture: catalogItem.picture,
-		isPublic: AppHelper.validateBooleanCliOptions(catalogItem.public, catalogItem.private),
-		registryId: catalogItem.registry-id,
-		images: [
-			{
-				containerImage: catalogItem.x86-image,
-				fogTypeId: 1
-			},
-			{
-				containerImage: catalogItem.arm-image,
-				fogTypeId: 2
-			}
-		],
-		inputType: {
-			infoType: catalogItem.input-type,
-			infoFormat: catalogItem.input-format
-		},
+const _createCatalogItemObject = function (catalogItem) {
+  return {
+    name: catalogItem.name,
+    description: catalogItem.description,
+    category: catalogItem.category,
+    configExample: catalogItem.config - example,
+    publisher: catalogItem.publisher,
+    diskRequired: catalogItem.disk - required,
+    ramRequired: catalogItem.ram - required,
+    picture: catalogItem.picture,
+    isPublic: AppHelper.validateBooleanCliOptions(catalogItem.public, catalogItem.private),
+    registryId: catalogItem.registry - id,
+    images: [
+      {
+        containerImage: catalogItem.x86 - image,
+        fogTypeId: 1
+      },
+      {
+        containerImage: catalogItem.arm - image,
+        fogTypeId: 2
+      }
+    ],
+    inputType: {
+      infoType: catalogItem.input - type,
+      infoFormat: catalogItem.input - format
+    },
 
-		inputType: catalogItem.input-type,
-    inputFormat: catalogItem.input-format,
-    outputType: catalogItem.output-type,
+    inputType: catalogItem.input - type,
+    inputFormat: catalogItem.input - format,
+    outputType: catalogItem.output - type,
     outputFormat: catalogItem.outputFormat
-	};
+  };
 };
-
 
 
 module.exports = new Catalog()
