9933b5bb8908805c68ffad9b689dc85dafbb18b9
stolbunov-da <da100pydoff@gmail.com>
2018-10-16 18:23:20 +0300

    registries (#196)
    * registries
    
    * registries fields fix

---
+++ src/controllers/registry-controller.js
@@ -0,0 +1,39 @@
+/*
+ * *******************************************************************************
+ *  * Copyright (c) 2018 Edgeworx, Inc.
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Eclipse Public License v. 2.0 which is available at
+ *  * http://www.eclipse.org/legal/epl-2.0
+ *  *
+ *  * SPDX-License-Identifier: EPL-2.0
+ *  *******************************************************************************
+ *
+ */
+
+const logger = require('../logger');
+const AuthDecorator = require('../decorators/authorization-decorator');
+const RegistryService = require('../services/registry-service');
+
+const createRegistryEndPoint = async function (req, user) {
+  logger.info("Parameters:" + JSON.stringify(req.body));
+  const registry = req.body;
+  await RegistryService.createRegistry(registry, user);
+};
+
+const getRegistriesEndPoint = async function (req, user) {
+  return await RegistryService.findRegistries(user);
+};
+
+const deleteRegistryEndPoint = async function (req, user) {
+  const deleteRegistry = {
+      id: parseInt(req.params.id)
+  }
+  return await RegistryService.deleteRegistry(deleteRegistry, user);
+};
+
+module.exports = {
+    createRegistryEndPoint: AuthDecorator.checkAuthToken(createRegistryEndPoint),
+    getRegistriesEndPoint: AuthDecorator.checkAuthToken(getRegistriesEndPoint),
+    deleteRegistryEndPoint: AuthDecorator.checkAuthToken(deleteRegistryEndPoint)
+};
