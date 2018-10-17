fa9fa5bf6d47c849fbe97f433100ed4b5dfee48e
Pankov <Oblivion!2345>
2018-10-17 19:48:54 +0300

    EWC-233 patch with not existing id

--- src/services/catalog-service.js
+++ src/services/catalog-service.js
@@ -163,7 +163,7 @@ const _checkForDuplicateName = async function (name, item, transaction) {
 const _checkIfItemExists = async function (where, transaction) {
   const item = await CatalogItemManager.findOne(where, transaction)
   if (!item) {
-    throw new Errors.NotFoundError(AppHelper.formatMessage(ErrorMessages.INVALID_CATALOG_ITEM_ID, id));
+    throw new Errors.NotFoundError(AppHelper.formatMessage(ErrorMessages.INVALID_CATALOG_ITEM_ID, where.id));
 
   }
   return item;
