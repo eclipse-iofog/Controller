5faa294afa398350e9967d73b9f814f577e02af6
ashpak <alexand.shpak@gmail.com>
2018-10-18 13:19:04 +0300

    controller endpoint

---
+++ src/controllers/controller.js
@@ -0,0 +1,34 @@
+/*
+ * *******************************************************************************
+ *  * Copyright (c) 2018 Edgeworx, Inc.
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Eclipse Public License v. 2.0 which is available at
+ *  * http://www.eclipse.org/legal/epl-2.0
+ *  *
+ *  * SPDX-License-Identifier: EPL-2.0
+ *  *******************************************************************************
+ *
+ */
+
+const ControllerService = require('../services/controller-service');
+
+
+const statusControllerEndPoint = async function (req) {
+  return await ControllerService.statusController(false);
+};
+
+const emailActivationEndPoint = async function (req) {
+  return await ControllerService.emailActivation(false);
+};
+
+const fogTypesEndPoint = async function (req) {
+  return await ControllerService.getFogTypes(false);
+};
+
+
+module.exports = {
+  statusControllerEndPoint: statusControllerEndPoint,
+  emailActivationEndPoint: emailActivationEndPoint,
+  fogTypesEndPoint: fogTypesEndPoint
+};
