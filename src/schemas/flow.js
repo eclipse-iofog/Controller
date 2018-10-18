08534aba4909d7e45f3c113ca615db55d8147970
Railag <whenthegroundcavedin@gmail.com>
2018-10-18 18:52:08 +0300

    flow CLI (#214)
    * flow CLI
    
    * swagger fix
    
    * cleanup

---
+++ src/schemas/flow.js
@@ -0,0 +1,25 @@
+const flowCreate = {
+  "id": "/flowCreate",
+  "type": "object",
+  "properties": {
+    "name": {"type": "string", "minLength": 1},
+    "description": {"type": "string"},
+    "isActive": {"type": "boolean"}
+  },
+  "required": ["name"]
+};
+
+const flowUpdate = {
+  "id": "/flowUpdate",
+  "type": "object",
+  "properties": {
+    "name": {"type": "string", "minLength": 1},
+    "description": {"type": "string"},
+    "isActive": {"type": "boolean"}
+  }
+};
+
+module.exports = {
+  mainSchemas: [flowCreate, flowUpdate],
+  innerSchemas: []
+};
