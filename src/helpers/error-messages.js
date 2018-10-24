ef496f1ebde5ec6e300d76ae9477dee51ca9e277
alexandershpak <35569337+alexandershpak@users.noreply.github.com>
2018-10-24 15:25:06 +0200

    Ashpak/feature diagnostic endpoints(snapshot) ewc 238 (#240)
    * updates on microservices endpoints
    
    * microservices endpoints update
    
    * EWC-222 in progress
    
    * fix create method
    
    * EWC-222 in progress
    
    * progress on microservices endpoints
    
    * EWC-222 wip
    
    * EWC-222 wip
    
    * update docs and db for microservices
    
    * update validation for microservices
    
    * fix create, update, get methods
    
    * EWC-222 strace done
    
    * EWC-222 swagger strace changes
    
    * Merge branch 'new-fog-controller' of https://github.com/ioFog/FogController into epankou/feature-diagnostic-endpoints-EWC-222
    
    # Conflicts:
    #	src/helpers/constants.js
    #	src/helpers/error-messages.js
    
    * Merge branch 'new-fog-controller' of https://github.com/ioFog/FogController into epankou/feature-diagnostic-endpoints-EWC-222
    
    # Conflicts:
    #	specs/swagger.yml
    #	src/helpers/error-messages.js
    
    * Merge branch 'new-fog-controller' of https://github.com/ioFog/FogController into epankou/feature-diagnostic-endpoints-EWC-222
    
    # Conflicts:
    #	specs/swagger.yml
    #	src/helpers/error-messages.js
    
    * image-snapshot end-point
    
    * image-snapshot end-point fix

--- src/helpers/error-messages.js
+++ src/helpers/error-messages.js
@@ -41,5 +41,7 @@ module.exports = {
   INVALID_MICROSERVICE: 'Microservice is not valid. Possible reasons: incorrect user, catalog item, flow or fog node.',
   INVALID_MICROSERVICE_CONFIG: "Can't create network microservice without appropriate configuration.",
   ROUTE_NOT_FOUND: 'Route not found',
-  INVALID_ACTION_PROPERTY: 'Unknown action property. Action can be "open" or "close"'
+  INVALID_ACTION_PROPERTY: 'Unknown action property. Action can be "open" or "close"',
+  IMAGE_SNAPSHOT_WITHOUT_FOG: 'Can not run image snapshot for element without fog.',
+  FILE_DOES_NOT_EXIST: 'File does not exist.'
 };
