4bd38c67357e2217d0d8e8a50c42076282d90046
Railag <whenthegroundcavedin@gmail.com>
2018-10-30 14:41:26 +0300

    fixed CLI error handling (#293)

--- src/cli/iofog.js
+++ src/cli/iofog.js
@@ -150,9 +150,9 @@ async function _executeCase(commands, commandName, f, isUserRequired) {
 
     if (isUserRequired) {
       const decoratedFunction = CliDecorator.prepareUserById(f);
-      decoratedFunction(obj);
+      await decoratedFunction(obj);
     } else {
-      f(obj);
+      await f(obj);
     }
   } catch (error) {
     logger.error(error.message);
