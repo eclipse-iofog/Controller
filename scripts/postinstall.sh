0d9245be648664489f6dbe48c03024c724c3e4fd
maksimchepelev <chepelev.maksim@gmail.com>
2018-11-08 16:22:18 +0300

    vars to /tmp folder correct comparison of versions

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,14 +1,55 @@
 #!/bin/bash
 
-PREV_IOFOG_CONTROLLER_VER=$(grep prev_ver scripts/temp/variables | awk '{print $2}')
+vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        echo '='
+        return
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            echo '>'
+            return
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            echo '<'
+            return
+        fi
+    done
+    echo '='
+    return
+}
 
-echo ${PREV_IOFOG_CONTROLLER_VER}
-if [[ "$PREV_IOFOG_CONTROLLER_VER" = "1.0.0" ]]; then
-    echo "Upgrading from ver 1.0.0"
-    sqlite3 src/sequelize/prod_database.sqlite "insert into SequelizeMeta (name) values ('20180928110125-insert-registry.js');"
-    sqlite3 src/sequelize/prod_database.sqlite "insert into SequelizeMeta (name) values ('20180928111532-insert-catalog-item.js');"
-    sqlite3 src/sequelize/prod_database.sqlite "insert into SequelizeMeta (name) values ('20180928112152-insert-iofog-type.js');"
-    sqlite3 src/sequelize/prod_database.sqlite "insert into SequelizeMeta (name) values ('20180928121334-insert-catalog-item-image.js');"
+PREV_IOFOG_CONTROLLER_VER=$(grep prev_ver /tmp/iofogcontroller_install_variables | awk '{print $2}')
+echo "Prev ver: "${PREV_IOFOG_CONTROLLER_VER}
+
+if [[ -z "${PREV_IOFOG_CONTROLLER_VER// }" ]]
+then
+    echo "No prev ver"
+else
+    if [[ $(vercomp $PREV_IOFOG_CONTROLLER_VER 1.0.0) = '<' ]] || [[ $(vercomp $PREV_IOFOG_CONTROLLER_VER 1.0.0) = '=' ]]
+    then
+        echo "Upgrading from ver 1.0.0"
+        sqlite3 src/sequelize/prod_database.sqlite "insert into SequelizeMeta (name) values ('20180928110125-insert-registry.js');"
+        sqlite3 src/sequelize/prod_database.sqlite "insert into SequelizeMeta (name) values ('20180928111532-insert-catalog-item.js');"
+        sqlite3 src/sequelize/prod_database.sqlite "insert into SequelizeMeta (name) values ('20180928112152-insert-iofog-type.js');"
+        sqlite3 src/sequelize/prod_database.sqlite "insert into SequelizeMeta (name) values ('20180928121334-insert-catalog-item-image.js');"
+    fi
 fi
 
-rm -rf scripts/temp
+rm -rf /tmp/iofogcontroller_install_variables
--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-mkdir scripts/temp
 export PREV_IOFOG_CONTROLLER_VER=$(npm list --depth=0 -g --silent | grep iofogcontroller | awk -F "@" '{print $2}')
-printf 'prev_ver: '$PREV_IOFOG_CONTROLLER_VER > scripts/temp/variables
+printf 'prev_ver: '$PREV_IOFOG_CONTROLLER_VER > /tmp/iofogcontroller_install_variables
