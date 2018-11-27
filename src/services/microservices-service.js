39ca9170aa1e7541105248492efdbbd55814d918
MaksimChepelev <chepelev.maksim@gmail.com>
2018-11-27 18:23:59 +0300

    EWC-371 validation of microservice uuid on update (#396)

--- src/services/microservices-service.js
+++ src/services/microservices-service.js
@@ -167,6 +167,9 @@ async function _updateMicroservice(microserviceUuid, microserviceData, user, isC
   const microserviceDataUpdate = AppHelper.deleteUndefinedFields(microserviceToUpdate);
 
   const microservice = await MicroserviceManager.findOne(query, transaction);
+  if (!microservice) {
+    throw new Errors.NotFoundError(AppHelper.formatMessage(ErrorMessages.INVALID_MICROSERVICE_UUID, microserviceUuid))
+  }
 
   if (microserviceDataUpdate.name) {
     const userId = isCLI ? microservice.userId : user.id;
