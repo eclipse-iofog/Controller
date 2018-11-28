3c69e28d2d9f82d2223f74325bee3a797a83bb72
Railag <whenthegroundcavedin@gmail.com>
2018-11-28 19:05:27 +0300

    EWC-378 fixed validation for email settings in config CLI

--- src/cli/config.js
+++ src/cli/config.js
@@ -182,10 +182,6 @@ const _addConfigOption = async function (options) {
   });
 
   await updateConfig(options.emailAddress, 'email-address', 'Email:Address', (onSuccess) => {
-    if (options.emailPassword) {
-      logger.info('When changing email address, password must be provided.');
-      return;
-    }
     config.set('Email:Address', options.emailAddress);
     onSuccess();
   });
