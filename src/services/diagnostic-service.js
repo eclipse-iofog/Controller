3aa596003c52e100e630e8113687ba3e60fe8354
maksimchepelev <chepelev.maksim@gmail.com>
2018-12-03 15:41:48 +0300

    EWC-390 cli `diagnostics strace-...` correct validation error messages

--- src/services/diagnostic-service.js
+++ src/services/diagnostic-service.js
@@ -15,7 +15,7 @@ const TransactionDecorator = require('../decorators/transaction-decorator');
 const AppHelper = require('../helpers/app-helper');
 const Errors = require('../helpers/errors');
 const ErrorMessages = require('../helpers/error-messages');
-const validator = require('../schemas/index');
+const Validator = require('../schemas/index');
 const MicroserviceService = require('../services/microservices-service');
 const StraceDiagnosticManager = require('../sequelize/managers/strace-diagnostics-manager');
 const ChangeTrackingService = require('./change-tracking-service');
@@ -29,7 +29,7 @@ const mime = require('mime');
 
 
 const changeMicroserviceStraceState = async function (id, data, user, isCLI, transaction) {
-  validator.validate(data, validator.schemas.straceStateUpdate);
+  await Validator.validate(data, Validator.schemas.straceStateUpdate);
   const microservice = await MicroserviceService.getMicroservice(id, user, isCLI, transaction);
   if (microservice.iofogUuid === null) {
     throw new Errors.ValidationError(ErrorMessages.STRACE_WITHOUT_FOG);
@@ -45,7 +45,7 @@ const changeMicroserviceStraceState = async function (id, data, user, isCLI, tra
 };
 
 const getMicroserviceStraceData = async function (id, data, user, isCLI, transaction) {
-  validator.validate(data, validator.schemas.straceGetData);
+  await Validator.validate(data, Validator.schemas.straceGetData);
   const straceData = await StraceDiagnosticManager.findOne({microserviceUuid: id}, transaction);
   const dir = config.get('Diagnostics:DiagnosticDir') || 'diagnostics';
   const filePath = dir + '/' + id;
@@ -65,7 +65,7 @@ const getMicroserviceStraceData = async function (id, data, user, isCLI, transac
 };
 
 const postMicroserviceStraceDatatoFtp = async function (id, data, user, isCLI, transaction) {
-  validator.validate(data, validator.schemas.stracePostToFtp);
+  await Validator.validate(data, Validator.schemas.stracePostToFtp);
   const straceData = await StraceDiagnosticManager.findOne({microserviceUuid: id}, transaction);
   const dir = config.get('Diagnostics:DiagnosticDir');
   const filePath = dir + '/' + id;
