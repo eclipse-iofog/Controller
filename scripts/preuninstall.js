a515d9bb1fa171423fd2e2322b4ab7c11fb3dbae
MaksimChepelev <chepelev.maksim@gmail.com>
2018-12-19 17:28:27 +0300

    feat(npm-scripts): npm scripts improvements (EWC-358 EWC-430)
    1. save configs during releases
    2. migrate to new encryption algorithm

--- scripts/preuninstall.js
+++ scripts/preuninstall.js
@@ -16,22 +16,22 @@ const execSync = require('child_process').execSync;
 const fs = require('fs');
 const version = require('../package').version;
 
-const rootDir = `${__dirname}/../`;
+const rootDir = `${__dirname}/..`;
 let installationVariablesFileName = 'iofogcontroller_install_variables';
 let installationVariablesFile;
 let tempDir;
 
 if (os.type() === 'Linux') {
-  tempDir = '/tmp/';
+  tempDir = '/tmp';
 } else if (os.type() === 'Darwin') {
-  tempDir = '/tmp/';
+  tempDir = '/tmp';
 } else if (os.type() === 'Windows_NT') {
-  tempDir = `${process.env.APPDATA}/`;
+  tempDir = `${process.env.APPDATA}`;
 } else {
   throw new Error("Unsupported OS found: " + os.type());
 }
 
-installationVariablesFile = tempDir + installationVariablesFileName;
+installationVariablesFile = tempDir + '/' + installationVariablesFileName;
 
 const instalationVars = {
   prevVer: version
@@ -41,11 +41,26 @@ fs.writeFileSync(installationVariablesFile, JSON.stringify(instalationVars));
 
 const devDb = `${rootDir}/src/sequelize/dev_database.sqlite`;
 if (fs.existsSync(devDb)) {
-  fs.renameSync(devDb, `${tempDir}dev_database.sqlite`)
+  fs.renameSync(devDb, `${tempDir}/dev_database.sqlite`)
 }
 
 const prodDb = `${rootDir}/src/sequelize/prod_database.sqlite`;
 if (fs.existsSync(prodDb)) {
-  fs.renameSync(prodDb, `${tempDir}prod_database.sqlite`)
+  fs.renameSync(prodDb, `${tempDir}/prod_database.sqlite`)
+}
+
+const defConfig = `${rootDir}/src/config/default.json`;
+if (fs.existsSync(defConfig)) {
+  fs.renameSync(defConfig, `${tempDir}/default_iofog_backup.json`)
+}
+
+const devConfig = `${rootDir}/src/config/development.json`;
+if (fs.existsSync(devConfig)) {
+  fs.renameSync(devConfig, `${tempDir}/development_iofog_backup.json`)
+}
+
+const prodConfig = `${rootDir}/src/config/production.json`;
+if (fs.existsSync(prodConfig)) {
+  fs.renameSync(prodConfig, `${tempDir}/production_iofog_backup.json`)
 }
 
