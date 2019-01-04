429bd3441d64fe114ad4602c658ba9f3ae0cc0ba
dbusel <10116634+dbusel@users.noreply.github.com>
2019-01-04 16:38:59 +0300

    Create postmantest.js

---
+++ scripts/postmantest.js
@@ -0,0 +1,23 @@
+/*
+ *  *******************************************************************************
+ *  * Copyright (c) 2018 Edgeworx, Inc.
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Eclipse Public License v. 2.0 which is available at
+ *  * http://www.eclipse.org/legal/epl-2.0
+ *  *
+ *  * SPDX-License-Identifier: EPL-2.0
+ *  *******************************************************************************
+ *
+ */
+ 
+ const newman = require('newman');
+ 
+// call newman.run to pass `options` object and wait for callback
+newman.run({
+    collection: require('../test/postman_collection.json.'),
+    reporters: 'cli'
+}, function (err) {
+    if (err) { throw err; }
+    console.log('Postman test complete!');
+});
