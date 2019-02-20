ebd007a577ecd8132fbccc2a41adaf0b6c60d853
Ian <ianmathieu@gmail.com>
2019-02-20 12:44:17 -0800

    Initial Pass on bringing all tests into scripts and automated, but lack of tests/variety and insight make it hard to judge what can and cannot be added

---
+++ scripts/testscript.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# This is to run automated testing within the Controller infrastructure, as well as local testing
+
+npm test
+npm run postman_test
+
+# Expand this as things become more available in local tests we have
