562372ca644b744177766b869424f09f71850e8d
Alex de Wergifosse <alex@edgeworx.io>
2020-04-14 15:49:06 +1200

    Fix update query

--- src/data/models/index.js
+++ src/data/models/index.js
@@ -5,7 +5,6 @@ const path = require('path')
 const Sequelize = require('sequelize')
 const lget = require('lodash/get')
 const constants = require('../constants')
-const CatalogItemImageManager = require('../managers/catalog-item-image-manager')
 const basename = path.basename(__filename)
 const db = {}
 const config = require('../../config')
@@ -32,15 +31,15 @@ Object.keys(db).forEach((modelName) => {
 db.sequelize = sequelize
 db.Sequelize = Sequelize
 
-const configureImage = async (manager, name, fogTypes, images, transaction) => {
-  const catalogItem = await manager.findOne({ where: { name, isPublic: false } }, transaction)
+const configureImage = async (db, name, fogTypes, images) => {
+  const catalogItem = await db.CatalogItem.findOne({ where: { name, isPublic: false } })
   for (const fogType of fogTypes) {
     if (fogType.id === 0) {
       // Skip auto detect type
       continue
     }
     const image = lget(images, fogType.id, '')
-    await CatalogItemImageManager.update({ fogTypeId: fogType.id, catalogItemId: catalogItem.id }, { containerImage: image })
+    await db.CatalogItemImage.update({ containerImage: image }, { where: { fogTypeId: fogType.id, catalogItemId: catalogItem.id } })
   }
 }
 
@@ -51,10 +50,9 @@ db.initDB = async () => {
   await databaseProvider.createUmzug(path.resolve(__dirname, '../seeders')).up()
 
   // Configure system images
-  const transaction = { fakeTransaction: true }
-  const fogTypes = await db.FogType.findAll({}, transaction)
-  await configureImage(db.CatalogItem, constants.ROUTER_CATALOG_NAME, fogTypes, config.get('SystemImages:Router', {}), transaction)
-  await configureImage(db.CatalogItem, constants.PROXY_CATALOG_NAME, fogTypes, config.get('SystemImages:Proxy', {}), transaction)
+  const fogTypes = await db.FogType.findAll({})
+  await configureImage(db, constants.ROUTER_CATALOG_NAME, fogTypes, config.get('SystemImages:Router', {}))
+  await configureImage(db, constants.PROXY_CATALOG_NAME, fogTypes, config.get('SystemImages:Proxy', {}))
 }
 
 module.exports = db
