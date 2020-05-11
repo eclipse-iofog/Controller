8aefbd4d58e46a6fba4cfac4797b551cb27f0f37
Alex de Wergifosse <alex@edgeworx.io>
2020-05-12 08:33:39 +1200

    Update copyrights

--- src/services/microservices-service.js
+++ src/services/microservices-service.js
@@ -1,6 +1,6 @@
 /* only "[a-zA-Z0-9][a-zA-Z0-9_.-]" are allowed
  * *******************************************************************************
- *  * Copyright (c) 2018 Edgeworx, Inc.
+ *  * Copyright (c) 2020 Edgeworx, Inc.
  *  *
  *  * This program and the accompanying materials are made available under the
  *  * terms of the Eclipse Public License v. 2.0 which is available at
