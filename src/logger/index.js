c4560ba3281db18632e916e9cacebf2b22d9cd5b
Saeid Baghbidi <srbaghbidi@yahoo.com>
2020-09-20 08:41:40 -0700

    Replaced winston logger with pino

--- src/logger/index.js
+++ src/logger/index.js
@@ -11,14 +11,12 @@
  *
  */
 
-const winston = require('winston')
-const config = require('../config')
 const fs = require('fs')
-const MESSAGE = Symbol.for('message')
+const path = require('path')
+const pino = require('pino')
+const config = require('../config')
 
 const dirName = config.get('Service:LogsDirectory')
-const maxSize = config.get('Service:LogsFileSize')
-const maxFiles = config.get('Service:LogsFileCount')
 
 // Create the log directory if it does not exist
 try {
@@ -30,93 +28,58 @@ try {
 }
 
 const levels = {
-  error: 0,
-  warn: 1,
-  cliReq: 2,
-  cliRes: 3,
-  apiReq: 4,
-  apiRes: 5,
-  info: 6,
-  verbose: 7,
-  debug: 8,
-  silly: 9
+  error: 100,
+  warn: 90,
+  cliReq: 80,
+  cliRes: 70,
+  apiReq: 60,
+  apiRes: 50,
+  info: 40,
+  verbose: 30,
+  debug: 20,
+  silly: 10
 }
 
-const formattedJson = winston.format((log) => {
-  log[MESSAGE] = JSON.stringify(log)
-  return log
-})
+const defaultFormat = {
+  level: 'info',
+  customLevels: levels,
+  useOnlyCustomLevels: true,
+  formatters: {
+    level: (level) => ({ level }),
+    log: (log) => {
+      if (!log.req) {
+        return log
+      }
 
-const prepareObjectLogs = winston.format((log) => {
-  if (!(log.message instanceof Object)) {
-    return log
+      return {
+        requestId: log.req.headers ? `[${log.req.headers['request-id']}] ` : '',
+        method: log.req.method,
+        args: {
+          params: log.req.params,
+          query: log.req.query,
+          body: log.req.body
+        }
+      }
+    }
   }
-
-  if (log.level === 'apiReq' && log.message instanceof Object) {
-    const req = log.message
-    const requestId = req.headers ? `[${req.headers['request-id']}] ` : ''
-    log.message = `${requestId}${req.method} ${req.originalUrl}`
-    log.args = { params: req.params, query: req.query, body: req.body }
-  }
-  if (log.level === 'apiRes' && log.message instanceof Object) {
-    const req = log.message.req
-    const res = log.message.res
-    const requestId = req.headers ? `[${req.headers['request-id']}] ` : ''
-    log.message = `${requestId}${req.method} ${req.originalUrl}`
-    log.args = res
-  }
-  return log
-})
-
-const logger = winston.createLogger({
-  levels: levels,
-  level: 'warn',
-  transports: [
-    new winston.transports.File({
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        prepareObjectLogs(),
-        formattedJson()
-      ),
-      filename: 'iofog-controller.log',
-      dirname: dirName,
-      maxsize: maxSize,
-      maxFiles: maxFiles
-    })
-  ]
-})
-
-// TODO: Fix test to pass without needing to set NODE_ENV to production
-// Tests are using console output to assess CLI command output
-if (process.env.NODE_ENV !== 'production2') {
-  logger.add(new winston.transports.Console({
-    level: 'info',
-    format: winston.format((log) => {
-      if (log.level === 'cliReq') {
-        return
-      }
-      if (log.level === 'apiReq' && log.message instanceof Object) {
-        const req = log.message
-        const requestId = req.headers ? `[${req.headers['request-id']}] ` : ''
-        log.message = `${requestId}${req.method} ${req.originalUrl}`
-        log.args = { params: req.params, query: req.query, body: req.body }
-      }
-      if (log.level === 'apiRes' && log.message instanceof Object) {
-        const req = log.message.req
-        const res = log.message.res
-        const requestId = req.headers ? `[${req.headers['request-id']}] ` : ''
-        log.message = `${requestId}${req.method} ${req.originalUrl}`
-        log.args = res
-      }
-      let message = log.level === 'cliRes' ? `${log.message}` : `[${log.level}] ${log.message}`
-
-      if (log.args) {
-        message += ` | args: ${JSON.stringify(log.args)}`
-      }
-      log[MESSAGE] = message
-      return log
-    })()
-  }))
 }
 
-module.exports = logger
+const consoleLogger = pino(defaultFormat)
+
+const logDestination = pino.destination(path.resolve(dirName, 'iofog-controller.log'))
+const fileLogger = pino(
+  {
+    ...defaultFormat,
+    level: 'warn'
+  },
+  logDestination)
+process.on('SIGHUP', () => logDestination.reopen())
+
+module.exports = {}
+
+for (const level of Object.keys(levels)) {
+  module.exports[level] = (...log) => {
+    consoleLogger[level](...log)
+    fileLogger[level](...log)
+  }
+}
