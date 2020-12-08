c713602cf451c14db07d248600573c5bdf51836a
Alex de Wergifosse <alex@edgeworx.io>
2020-12-09 11:40:58 +1300

    Update HTTP return code for PUT /applicationTemplate

--- src/routes/applicationTemplate.js
+++ src/routes/applicationTemplate.js
@@ -135,7 +135,7 @@ module.exports = [
     middleware: async (req, res) => {
       logger.apiReq(req)
 
-      const successCode = constants.HTTP_CODE_NO_CONTENT
+      const successCode = constants.HTTP_CODE_SUCCESS
       const errorCodes = [
         {
           code: constants.HTTP_CODE_BAD_REQUEST,
